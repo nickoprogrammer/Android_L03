b1b4be0f053f1dd678c0e572d3c95e9c8e1b5536
Nickoprogrammer <nickoprogrammer@gmail.com>
2024-03-25 11:45:56 +0200

    Added button to count the times' pressed

--- app/src/main/java/com/example/lesson3_androidapp/MainActivity.java
+++ app/src/main/java/com/example/lesson3_androidapp/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.lesson3_androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private TextView textView;
+
+    private int numTimesClicked = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button3);
+        textView = findViewById(R.id.textView);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numTimesClicked++;
+                String result = "\n The button clicked " +numTimesClicked + " times";
+                textView.append(result);
+
+            }
+        };
+
+        button.setOnClickListener(listener);
+
     }
 }
